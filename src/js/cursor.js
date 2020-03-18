5c1ea446de9eaae26c2f5656405986f3ff668611
SG-WebDev <sgrobelny.contact@gmail.com>
2020-03-18 22:42:13 +0100

    Add cursor element to HTML, Get hover elements from client Add mouse event to hover items Add hover size and transparency params in CSS

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -1,16 +1,40 @@
-const cursorElement = document.querySelector('.motionPointer');
-console.log(cursorElement)
-// event handler function
-function handler(e) {
-    let pageX = e.pageX;
-    let pageY = e.pageY;
-    cursorElement.style.transform = `translate(${pageX}px, ${pageY}px)`;
-    // IE 8
-    if (pageX === undefined) {
-        pageX = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-        pageY = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-    }
 
-    console.log(pageX, pageY);
+function motionPointerInit(arr) {
+    // HTML cursor classes
+    const baseCursorClass = 'motionPointer';
+    const hoverCursorClass = 'motionPointer--hover';
+
+    // create cursor element
+    let newCursor = document.createElement('div');
+    newCursor.classList.add(baseCursorClass);
+    document.querySelector('body').appendChild(newCursor);
+    const cursorElement = document.querySelector('.motionPointer');
+
+    // get DOM hover elements
+    let domItems = [];
+    arr.forEach( arrEl => {
+        let domItem= document.querySelectorAll(arrEl);
+        domItems.push(domItem);
+        return domItems;
+    });
+
+    // add mouse events to hover elements
+    domItems.forEach( nodeItems => {
+        nodeItems.forEach(nodeItem => {
+            nodeItem.addEventListener('mouseenter', function () {
+                cursorElement.classList.add(hoverCursorClass);
+            });
+            nodeItem.addEventListener('mouseleave', function () {
+                cursorElement.classList.remove(hoverCursorClass);
+            });
+        });
+    });
+
+    // change mouse position function
+    function handler(e) {
+        let pageX = e.pageX;
+        let pageY = e.pageY;
+        cursorElement.style.transform = `translate(${pageX}px, ${pageY}px)`;
+    }
+    document.addEventListener('mousemove', handler);
 }
-document.addEventListener('mousemove', handler);
